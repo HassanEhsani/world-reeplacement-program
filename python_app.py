547ee9aa852907e9fdd88ef2f6ca88d0507746cf
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-12 22:20:23 +0300

    runed

--- python_app.py
+++ python_app.py
@@ -1,3 +1,8 @@
 def replace_world():
     str = "Hi guys, i am Hassan, and hi hi hi"
-    world_to_replace = input("Enter the word to replace")
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacment = input("Enter the word replacment: ")
+    print(str.replace(word_to_replace,word_replacment))
+
+replace_world()
+
